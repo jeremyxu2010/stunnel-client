263f53452e4d69205f28ab80d8398dfa55445135
Jeremy Xu <jeremyxu2010@gmail.com>
2013-11-14 14:39:11 +0800

    Initial Commit

---
+++ cpp-stunnel/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp-stunnel
+//
+//  Created by Jeremy Xu on 13-11-14.
+//  Copyright (c) 2013年 Jeremy Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
